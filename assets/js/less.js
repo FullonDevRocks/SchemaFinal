c0c5c6cff92b4fbd2b8f093af964d23214bd65b5
FullonDevRocks <101067037+FullonDevRocks@users.noreply.github.com>
2022-04-27 23:03:12 +0530

    yes

--- assets/js/less.js
+++ assets/js/less.js
@@ -1,7 +1,17 @@
 
 $(document).ready(()=> {
-    function openNav() {
-        document.getElementById("mySidebar").style.width = "300px";
+    let btnMenuSwitch = document.getElementById("MenuSwitch");
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+          console.log("width min 2500");
+          openNav("25%");
+        } else {
+            console.log("width min not 2500");
+            openNav("300px")
+        }
+    }
+    function openNav(sz) {
+        document.getElementById("mySidebar").style.width = sz;
         // document.getElementById("").style.marginLeft = "250px";
     }
   
@@ -10,12 +20,13 @@ $(document).ready(()=> {
         document.getElementById("mySidebar").style.width = "0";
         // document.getElementById("").style.marginLeft = "0";
     }
+    var x = window.matchMedia("(min-width: 2500px)");
     console.log("working");
-    let btnMenuSwitch = document.getElementById("MenuSwitch");
+    
     btnMenuSwitch.addEventListener('click', function(){
         console.log(btnMenuSwitch.checked);
         if (btnMenuSwitch.checked){
-            openNav();
+            myFunction(x);
 
         } else {
             closeNav();
@@ -23,4 +34,10 @@ $(document).ready(()=> {
     })
 })
 
+
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
+  
 
